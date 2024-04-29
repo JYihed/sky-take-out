5c888c63acea0f9a121be0eac782cfc91bc38f59
chenyi <13433089+chenyibehappy@user.noreply.gitee.com>
2024-04-29 12:45:48 +0800

    店铺状态管理业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,64 @@
+package com.sky.service;
+
+import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
+
+import java.util.List;
+
+public interface SetmealService {
+
+
+
+
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void save(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult page(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(SetmealDTO setmealDTO);
+
+
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 删除套餐
+     * @param ids
+     * @return
+     */
+    void deleteBatch(List<Long> ids);
+}
