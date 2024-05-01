58f494ea348f53bb22d4c7ef21980bd15eade3e3
chenyi <13433089+chenyibehappy@user.noreply.gitee.com>
2024-05-01 12:21:19 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,26 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.ShoppingCart;
+import com.sky.enumeration.OperationType;
+import com.sky.service.ShoppingCartService;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+
+
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time,number) VALUES " +
+            "(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{amount},#{createTime},#{number})")
+    void insert(ShoppingCart shoppingCart);
+}
