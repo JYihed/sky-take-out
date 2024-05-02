67ef86703f0daa4c7e51c6aeb3d78e475d447ce7
chenyi <13433089+chenyibehappy@user.noreply.gitee.com>
2024-05-02 14:57:35 +0800

    C端用户下单业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "用户端订单相关接口")
+@RequestMapping("/user/order")
+@RestController("userOrderController")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+    /**
+     * 用户下单
+     * @param submitDTO
+     * @return
+     */
+    @ApiOperation("用户下单")
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO submitDTO){
+        OrderSubmitVO submitVO = orderService.submitOrder(submitDTO);
+        return Result.success(submitVO);
+    }
+}
